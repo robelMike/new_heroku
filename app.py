9f4859f3ac904bc3fd53d98da599389aa0182190
robelMike <robelmichael102@gmail.com>
2020-03-01 18:55:02 +0100

    new

--- app.py
+++ app.py
@@ -4,19 +4,23 @@ from flask_restful import Resource, Api
 from flask_celery import make_celery
 import requests
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from db import db
+
 
 
 app = Flask(__name__)
 
 app.config['CELERY_BROKER_URL'] = 'amqp://jdcsohky:6n7Niqm9-hEtQiUWg912u0EWzCt-bLE_@crow.rmq.cloudamqp.com/jdcsohky'
-app.config['CELERY_BACKEND'] = 'rpc://'
+app.config['CELERY_BACKEND'] = 'db+postgres://liwrnidwmrvjrl:dd967c421553a2bafb708b4865c8bb5c36461ee00a2f8b2971ae19b96afc29b6@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/dd0178iv33fmmj'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 celery = make_celery(app)
 db = SQLAlchemy(app)
 app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'super secret key'
+
 
 new_list = []
 
@@ -43,24 +47,34 @@ class fixdb(db.Model):
 def create_tables():
 	db.create_all()
 """
-@app.route('/get_temp', methods=['GET'])
+@celery.task(name='dht.receive')
 def receive_dht():
-	r = requests.get("http://192.168.0.34:5000/dht")
-	data = r.json()
-	temp = data['temperature']
-	name = data['humidity']
+	get_receive()	
+	return 'ok'
+
+
+def get_receive():
+	#r = requests.get("http://192.168.0.34:5000/dht")
+	#data = r.json()
+
+	#temp = data['temperature']
+	#name = data['humidity']
+	temp = 12
+	name = 44
 	print(f"temp: {temp} hum: {name}")
 	new_temp = temp
 	new_name = name
-	session['new_temp'] = new_temp
-	session['new_name'] = new_name
-	print(temp)
-	print(name)
+	#url = "http://0.0.0.0:5000/temp"
+	#session['new_temp'] = new_temp
+	#session['new_name'] = new_name
+	#r = requests.post(url, temp)
+	#print(r.text)
 	print(f"new_list: {new_list}")
 	return 'ok'
 
 @app.route('/create', methods=['GET'])
 def postrandom():
+	receive_dht.delay()
 	temp = session.get('new_temp')
 	name = session.get('new_name')
 	print(f"temp i create: {temp}")
